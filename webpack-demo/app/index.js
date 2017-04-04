c0f5c922078d0037d2c5d9e3035b1193ee7a8721
ekkoJ <270114440@qq.com>
2017-04-04 17:39:06 +0800

    update

--- webpack-demo/app/index.js
+++ webpack-demo/app/index.js
@@ -17,63 +17,65 @@ const timer = setInterval(finishImgDir,60);
 
 function finishImgDir() {
     if (imgArr.length === imageDir.length - 1) {
-        getCanvasInfo(imgArr);
+        // getCanvasInfo(imgArr); //瀑布流布局
+        getCanvasInfo2(imgArr); //序列帧布局
         clearInterval(timer);
         return;
     } 
 }
-// function getCanvasInfo(arr) { //瀑布流布局
-//     const sqrt = Math.ceil(Math.sqrt(arr.length));
-//     let heightArr = Array(sqrt).fill(0);
-//     let offsetArr = [];
-//     let xline, yline, x_pos, maxHeight, maxWidth;
-//     let turns = 0;
-//     let x_offset = 20;
-//     let y_offset = 20;
-//     let newImage = images(100,100);
 
-//     arr.forEach((item, index) => {
-//         const hIndex = heightArr.indexOf(Math.min.apply(null,heightArr));
-//         const oWidth = 500;
+function getCanvasInfo(arr) { //瀑布流布局
+    const sqrt = Math.ceil(Math.sqrt(arr.length));
+    let heightArr = Array(sqrt).fill(0);
+    let offsetArr = [];
+    let xline, yline, x_pos, maxHeight, maxWidth;
+    let turns = 0;
+    let x_offset = 20;
+    let y_offset = 20;
+    let newImage = images(100,100);
 
-//         heightArr[hIndex] = heightArr[hIndex] === 0 ?
-//               Number(images(item).size(oWidth).height()) :
-//               heightArr[hIndex] + Number(images(item).size(oWidth).height()) + 20;
-//         x_pos = (oWidth + x_offset) * hIndex;
+    arr.forEach((item, index) => {
+        const hIndex = heightArr.indexOf(Math.min.apply(null,heightArr));
+        const oWidth = 500;
 
-//         this.offsetObj = {
-//             x: x_pos,
-//             y: heightArr[hIndex] - Number(images(item).size(oWidth).height()),
-//             image_path: item
-//         }
+        heightArr[hIndex] = heightArr[hIndex] === 0 ?
+              Number(images(item).size(oWidth).height()) :
+              heightArr[hIndex] + Number(images(item).size(oWidth).height()) + 20;
+        x_pos = (oWidth + x_offset) * hIndex;
 
-//         offsetArr.push(this.offsetObj)
+        this.offsetObj = {
+            x: x_pos,
+            y: heightArr[hIndex] - Number(images(item).size(oWidth).height()),
+            image_path: item
+        }
 
-//         ++ turns;
-//         if( turns === arr.length) {
-//             maxHeight = Math.max.apply(null,heightArr);
-//             maxWidth = Number(oWidth * sqrt + x_offset * (sqrt - 1));
-//             newImage.size(maxWidth,maxHeight);
-//             mergeImage(offsetArr, newImage);
-//             // newImage.save("output.png");
-//         }
-//     })
+        offsetArr.push(this.offsetObj)
+
+        ++ turns;
+        if( turns === arr.length) {
+            maxHeight = Math.max.apply(null,heightArr);
+            maxWidth = Number(oWidth * sqrt + x_offset * (sqrt - 1));
+            newImage.size(maxWidth,maxHeight);
+            mergeImage(offsetArr, newImage);
+            // newImage.save("output.png");
+        }
+    })
    
-// }
+}
 
-// function mergeImage(arr, newImage) {
-//     turns = 0;
-//     arr.forEach((item, index) => {
-//         newImage.draw(images(item.image_path).size(500),item.x,item.y);
-//         ++ turns;
-//         if (turns === arr.length) {
-//             newImage.save("output.png");
-//             console.log('======done======');
-//         }
-//     })
-// }
+function mergeImage(arr, newImage) {
+    turns = 0;
+    arr.forEach((item, index) => {
+        newImage.draw(images(item.image_path).size(500),item.x,item.y);
+        ++ turns;
+        if (turns === arr.length) {
+            newImage.save("output.png");
+            console.log('======done======');
+        }
+    })
+}
 
-function getCanvasInfo(arr) { //序列帧布局
+function getCanvasInfo2(arr) { //序列帧布局
     
     const oWidth = 500;
     const picPix = 0.66; //图片宽高比
